ec9718f7e13fd725294367f89543b4c00ed7fce3
System-tm32 <Systemtm32@yandex.ru>
2018-03-18 17:33:50 +0300

    upd

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	 $('.owl-carousel').owlCarousel({
+	 	items: 1,
+	    loop: true,
+	    dots: false,
+	    margin: 0,
+	    nav: true,
+	    navText: ["<i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i>","<i class=\"fa fa-arrow-right\" aria-hidden=\"true\"></i>"]
+	});
+
+});
